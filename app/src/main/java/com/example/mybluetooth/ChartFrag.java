6a9a8b752aeb3b6c9fc145d64064d37bdf075516
Geurin <stumbletrip88@gmail.com>
2022-05-29 21:59:50 +0900

    commit
    0529) 차트 기능 업데이트

--- app/src/main/java/com/example/mybluetooth/ChartFrag.java
+++ app/src/main/java/com/example/mybluetooth/ChartFrag.java
@@ -34,11 +34,13 @@ import java.util.List;
 public class ChartFrag extends Fragment {
     private View view;
 
-    private LineChart lineChart;
-    ArrayList<Entry> entry_chart = new ArrayList<>();
-
     private Handler mHandler;
+
+    //Chart
+    private LineChart lineChart;
     private List<Float> temp_values = new ArrayList<>();
+
+    private Thread thread;
 
     private TextView bufferText;
 
@@ -51,21 +53,11 @@ public class ChartFrag extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.chartfrag,container,false);
 
-        lineChart = (LineChart) view.findViewById(R.id.linechart);//layout의 id
-        lineChart.setHighlightPerDragEnabled(true);
-        lineChart.setTouchEnabled(true);
-        lineChart.setDragEnabled(true);
-        lineChart.setScaleEnabled(true);
-        lineChart.setDrawGridBackground(false);
-        lineChart.setPinchZoom(true);
-        lineChart.setBackgroundColor(Color.LTGRAY);
-
         bufferText = (TextView) view.findViewById(R.id.buffertext);
 
-        //temp_values.add(0f);
+        temp_values.add(0f);
 
-        mHandler=new Handler(){
-            @Override
+        mHandler = new Handler(){
             public void handleMessage(Message msg){
                 if(msg.what == MESSAGE_READ){
                     String readMessage = null;
@@ -75,42 +67,102 @@ public class ChartFrag extends Fragment {
                     catch (UnsupportedEncodingException e) {
                         e.printStackTrace();
                     }
-                    temp_values.add(Float.parseFloat(readMessage));
-                    bufferText.setText(readMessage);
+                    String[] array = readMessage.split(",");
+                    temp_values.add(Float.parseFloat(array[0]));
+                    //float temp_values = Float.parseFloat(array[0]);
+                    bufferText.setText(array[0]);
                 }
             }
         };
 
-        LineData chartData = new LineData();
-        if(temp_values.size() > 0){
-            for (int i = 0; i < temp_values.size(); i++) {
-                entry_chart.add(new Entry(i, temp_values.get(i)));
-            }
-        }
-        else{
-            for (int i = 0; i < 10; i++) {
-                float val = (float) (Math.random() * 10);
-                entry_chart.add(new Entry(i, val));
-            }
-        }
+        lineChart = (LineChart) view.findViewById(R.id.linechart);//layout의 id
+        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
 
-        LineDataSet lineDataSet = new LineDataSet(entry_chart, "temp");
-        chartData.addDataSet(lineDataSet);
-
-        // lineDataSet.setColor(Color.BLACK);
-        //lineDataSet.setCircleColor(Color.BLACK);
-
-        lineDataSet.setFillAlpha(65);
-        lineDataSet.setColor(Color.WHITE);
-        lineDataSet.setCircleColor(Color.WHITE);
-        lineDataSet.setLineWidth(2f);
-        lineDataSet.setDrawValues(false);
-        lineDataSet.setValueTextColor(Color.WHITE);
-
-        lineChart.setData(chartData);
-
+        lineChart.getAxisRight().setEnabled(false);
+        lineChart.getLegend().setTextColor(Color.WHITE);
+        lineChart.animateXY(2000, 2000);
         lineChart.invalidate();
 
+        LineData data = new LineData();
+        lineChart.setData(data);
+
+        feedMultiple();
         return view;
+    }
+
+    private void addEntry() {
+        LineData data = lineChart.getData();
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+
+            //data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
+            data.addEntry(new Entry(set.getEntryCount(),temp_values.get(temp_values.size()-1)), 0);
+            data.notifyDataChanged();
+
+            lineChart.notifyDataSetChanged();
+            lineChart.setVisibleXRangeMaximum(10);
+            lineChart.moveViewToX(data.getEntryCount());
+        }
+    }
+
+    private LineDataSet createSet() {
+
+        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        set.setFillAlpha(110);
+        set.setFillColor(Color.parseColor("#d7e7fa"));
+        set.setColor(Color.parseColor("#0B80C9"));
+        set.setCircleColor(Color.parseColor("#FFA1B4DC"));
+        set.setValueTextColor(Color.WHITE);
+        set.setDrawValues(false);
+        set.setLineWidth(2);
+        set.setCircleRadius(6);
+        set.setDrawCircleHole(false);
+        set.setDrawCircles(false);
+        set.setValueTextSize(9f);
+        set.setDrawFilled(true);
+
+        set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set.setHighLightColor(Color.rgb(244, 117, 117));
+
+        return set;
+    }
+
+    private void feedMultiple() {
+        if (thread != null)
+            thread.interrupt();
+
+        final Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                addEntry();
+            }
+        };
+
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    getActivity().runOnUiThread(runnable);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ie) {
+                        ie.printStackTrace();
+                    }
+                }
+            }
+        });
+        thread.start();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (thread != null)
+            thread.interrupt();
     }
 }

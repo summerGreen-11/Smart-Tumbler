98e8923b329ec9581edcac4cd4e8870d4c418b0e
Geurin <stumbletrip88@gmail.com>
2022-07-25 03:10:07 +0900

    commit

--- app/src/main/java/com/example/mybluetooth/HomeFrag.java
+++ app/src/main/java/com/example/mybluetooth/HomeFrag.java
@@ -1,9 +1,11 @@
 package com.example.mybluetooth;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,11 +14,38 @@ import androidx.fragment.app.Fragment;
 public class HomeFrag extends Fragment {
     private View view;
 
+    //DB 연동
+    private SensorDBHelper dbHelper;
+    private TextView testPrint;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.homefrag,container,false);
 
+        //테스트 데이터 출력
+        testPrint = (TextView) view.findViewById(R.id.dataPrintTest);
+
+        getDBdata();
+
         return view;
+    }
+
+    private void getDBdata() {
+        Cursor cursor = dbHelper.readRecord();
+
+        while (cursor.moveToNext()) {
+            int itemId = cursor.getInt(cursor.getColumnIndexOrThrow(SensorContract.SensorEntry._ID));
+            int temp = cursor.getInt(cursor.getColumnIndexOrThrow(SensorContract.SensorEntry.COLUMN_TEMP));
+            int weight = cursor.getInt(cursor.getColumnIndexOrThrow(SensorContract.SensorEntry.COLUMN_WEIGHT));
+            String colordt = cursor.getString(cursor.getColumnIndexOrThrow(SensorContract.SensorEntry.COLUMN_COLOR));
+
+            String result = Integer.toString(itemId) + ": "+Integer.toString(temp) + "도, "+
+                    Integer.toString(weight) + "g, 음료: "+colordt;
+
+            testPrint.setText(result);
+        }
+
+        cursor.close();
     }
 }

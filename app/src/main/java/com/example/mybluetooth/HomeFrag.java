3cec9c631ed2500cd64e66dff7778402adb47675
Geurin <stumbletrip88@gmail.com>
2022-08-12 16:12:25 +0900

    add layout page
    - 화면 레이아웃 디자인 추가

--- app/src/main/java/com/example/mybluetooth/HomeFrag.java
+++ app/src/main/java/com/example/mybluetooth/HomeFrag.java
@@ -7,11 +7,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
 
 public class HomeFrag extends Fragment {
     private View view;
@@ -20,27 +23,24 @@ public class HomeFrag extends Fragment {
     private SensorDBHelper dbHelper;
     private TextView testPrint;
 
+    private TextView currentTemp;
+    private ImageView waterImg;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.homefrag,container,false);
 
-        //테스트 데이터 출력
+        //현재 온도
+        currentTemp = (TextView) view.findViewById(R.id.current_temp);
+        //섭취량 이미지
+        waterImg = (ImageView) view.findViewById(R.id.water_homf_img);
+
+
+        //테스트 데이터 출력 전 세팅
         testPrint = (TextView) view.findViewById(R.id.dataPrintTest);
-
         dbHelper = new SensorDBHelper(getActivity().getApplicationContext());
-
-        try {
-            getDBdata();
-        }
-        catch(Exception e){
-            testPrint.setText("DB NULL");
-        }
-
-        return view;
-    }
-
-    private void getDBdata() {
+        //데이터 출력
         try {
             Cursor cursor = dbHelper.readRecord();
 
@@ -54,11 +54,19 @@ public class HomeFrag extends Fragment {
                         Integer.toString(temp) + "도, 무게: " +
                         Integer.toString(weight) + "g, 음료: " + colordt;
                 testPrint.setText(result);
+
+                //현재 온도 출력
+                currentTemp.setText(temp);
             }
-                cursor.close();
+            cursor.close();
         }
         catch (Exception e){
             testPrint.setText("DATA EMPTY");
         }
+
+
+
+
+        return view;
     }
 }

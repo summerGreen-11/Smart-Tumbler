fe1b38b0d6d254f6c4f61045e25c38389289c76f
Geurin <stumbletrip88@gmail.com>
2022-08-20 21:07:00 +0900

    add chart code

--- app/src/main/java/com/example/mybluetooth/ChartFrag.java
+++ app/src/main/java/com/example/mybluetooth/ChartFrag.java
@@ -9,6 +9,7 @@ import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.charts.LineChart;
@@ -38,14 +39,16 @@ import androidx.fragment.app.Fragment;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import java.io.UnsupportedEncodingException;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 
 public class ChartFrag extends Fragment {
     private View view;
 
     //MPAndroidChart
-    private LineChart lineChart;
     private LineDataSet setComp1;
     //DB 연동
     private SensorDBHelper dbHelper;
@@ -69,13 +72,6 @@ public class ChartFrag extends Fragment {
 
         //DB 처리
         dbHelper = new SensorDBHelper(getActivity().getApplicationContext());
-        
-//        //차트 설정
-//        lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
-//        lineChart.getDescription().setEnabled(false);
-//        lineChart.getAxisRight().setEnabled(false);
-//        lineChart.getLegend().setTextColor(Color.WHITE);
-//        lineChart.animateXY(5000, 5000);
 
         //일별 차트 버튼
         DayBtn = (Button) view.findViewById(R.id.btn_chart_day);
@@ -87,33 +83,32 @@ public class ChartFrag extends Fragment {
                 xVals.clear();
 
                 SQLiteDatabase sql = dbHelper.getReadableDatabase();
-                Cursor cursor = sql.rawQuery("SELECT Date('SensorData.dateTime') AS date FROM SensorData GROUP BY date", null);
-
+                Cursor cursor = sql.rawQuery("SELECT strftime(\"%d\", today) AS date FROM SensorData GROUP BY date", null);
                 String luxvalue = "DB" + "\r\n";
 
                 while (cursor.moveToNext()) {
+
                     valsComp1.add(new Entry(Float.parseFloat(cursor.getString(1)), Integer.parseInt(cursor.getString(0))));
-                    xVals.add("" + cursor.getString(0));
+                    xVals.add(cursor.getString(0));
                     luxvalue += cursor.getString(0) + " " + cursor.getString(1) + "\r\n";
                 }
-
                 cursor.close();
                 sql.close();
 
+                setComp1 = new LineDataSet(valsComp1, "조도 값");
 
+                ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();       //최종적으로 그래프에 추가할 dataSets 설정
+                dataSets.add(setComp1);                                     //위에서 적용한 그래프 모양 + 데이터를 dataSets에 넣음
+
+                LineData data = new LineData(dataSets);
+                LineChart chart = (LineChart) view.findViewById(R.id.linechart);
+                chart.setData(data);
+                chart.getAxisRight().setDrawLabels(false);
+                chart.invalidate();
             }
         });
 
-        lineChart.invalidate();
-
-        LineData data = new LineData();
-        lineChart.setData(data);
-
-        lineChart.notifyDataSetChanged();
-        lineChart.setVisibleXRangeMaximum(5);
-        lineChart.moveViewToX(data.getEntryCount());
-
-//        feedMultiple();
         return view;
     }
+
 }
